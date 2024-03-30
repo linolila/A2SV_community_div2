6c17aa346864c8279c14c41d8f1b9d151a37e1d6
Jitu_Tesfaye <jitutesfaye17@gmail.com>
2024-03-30 15:58:46 +0300

    Create number_of_smaller.py

---
+++ number_of_smaller.py
@@ -0,0 +1,14 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(n -1):
+            if nums[i] == nums[i+1]:
+               nums[i]*=2
+               nums[i+1] = 0
+               i+=1
+        j = 0 
+        for i in range(n):
+            if nums[i] !=0 :
+                nums[i] , nums[j] = nums[j] ,nums[i]
+                j+=1
+        return nums
