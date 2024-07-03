63814d6ad0c6f8e4c109c87344611ecc8c424eb1
Jitu_Tesfaye <jitutesfaye17@gmail.com>
2024-07-03 11:31:03 +0300

    Update nextGreatestLetter.py

--- nextGreatestLetter.py
+++ nextGreatestLetter.py
@@ -4,3 +4,15 @@ class Solution:
             if letter > target :
                 return letter
          return letters[0]
+        class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        l , r = 0 , len(letters) -1
+        if target < letters[l] or target >= letters[r]:
+            return letters[l]
+        while l <= r :    
+            mid = (l + r) // 2
+            if target < letters[mid] :
+                r = mid - 1
+            else :
+                l = mid + 1
+        return letters[l]
