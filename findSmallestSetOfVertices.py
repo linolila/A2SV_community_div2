a0efd770a65f64f500ed0fde6d7e48658308aabf
Jitu_Tesfaye <jitutesfaye17@gmail.com>
2024-08-18 16:42:25 +0300

    Create findSmallestSetOfVertices.py

---
+++ findSmallestSetOfVertices.py
@@ -0,0 +1,10 @@
+class Solution:
+        def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:       
+            in_degree = [0] * n
+            for u, v in edges:
+                in_degree[v] += 1
+            result = [i for i in range(n) if in_degree[i] == 0]
+                
+            return result
+
+                    
